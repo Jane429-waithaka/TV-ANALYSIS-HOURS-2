2f2b15c9dfa458634e00b728c1ce78d355801455
Jane429-waithaka <janewaithaka429@gmail.com>
2026-03-16 15:35:29 +0300

    Rename TV_HOURS_TABLE to TV_HOURS_TAB

--- MAIN.R
+++ MAIN.R
@@ -1,7 +1,7 @@
 gss_cat
-TV_HOURS_TABLE<-gss_cat %>% 
+TV_HOURS_TAB<-gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>%
   summarise(mean_tv_hours= mean(tvhours,na.rm=TRUE))
 
-write.csv(TV_HOURS_TABLE,here("Tv_hours_by_marital.csv"))
+write.csv(TV_HOURS_TAB,here("Tv_hours_by_marital.csv"))
