4f081804b563fb8b1f2ec6c9426d6e5601e1f4b0
Jane429-waithaka <janewaithaka429@gmail.com>
2026-03-16 15:36:49 +0300

    MAKE NAMES LONGER

--- MAIN.R
+++ MAIN.R
@@ -1,7 +1,7 @@
 gss_cat
-TV_HOURS_TABLE<-gss_cat %>% 
+TV_HOURS_TABULATION<-gss_cat %>% 
   filter(age < 30) %>% 
   group_by(marital) %>%
   summarise(mean_tv_hours= mean(tvhours,na.rm=TRUE))
 
-write.csv(TV_HOURS_TABLE,here("Tv_hours_by_marital.csv"))
+write.csv(TV_HOURS_TABULATION,here("Tv_hours_by_marital.csv"))
